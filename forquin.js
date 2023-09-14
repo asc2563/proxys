2c745f5eccd5e6e6c3e362c0fa4f8e328e08a03e
asc2563 <120886940+asc2563@users.noreply.github.com>
2023-09-14 15:20:18 -0700

    Update forquin.js

--- forquin.js
+++ forquin.js
@@ -1,5 +1,5 @@
   win = window.open();
-    url = 'https://101010000101001gdfsgfshgdhjs.gameubg.com/';
+    url = 'https://2vhs5z-1234.csb.app/';
     iframe = win.document.createElement('iframe');
     iframe.style = 'position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;';
     iframe.src = url;
