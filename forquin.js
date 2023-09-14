c593bf69a3488b0b98956152433bf3b3863181c2
asc2563 <120886940+asc2563@users.noreply.github.com>
2023-09-14 15:13:47 -0700

    don’t read this

---
+++ forquin.js
@@ -0,0 +1,7 @@
+  win = window.open();
+    url = 'https://101010000101001gdfsgfshgdhjs.gameubg.com/';
+    iframe = win.document.createElement('iframe');
+    iframe.style = 'position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;';
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+`;
