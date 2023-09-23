78cec426b4a485478450cbcb59ba505e5a8c0c39
asc2563 <120886940+asc2563@users.noreply.github.com>
2023-09-23 16:20:56 -0700

    added tiny url

--- proxy.js
+++ proxy.js
@@ -1,5 +1,5 @@
 var option = prompt(
-  "what would you like to do\n\n1): launch gameubg\n2): launch gogo private\n3): launch rblocker\n4): calculator\n5): tab cloak\n6): history flood\n7): clever cloak\n8): launch 3 proxys\n9): launch nebula\n10): source code\n11): proxy doc\n12): URL proxy\n13): proxy all in one\n15): launch Aluben\n16): launch procky list\n17): launch Aluben hub\n18): rename tab\n19:) launch ultraviolet\n20): launch holy ub"
+  "what would you like to do\n\n1): launch gameubg\n2): launch gogo private\n3): launch rblocker\n4): calculator\n5): tab cloak\n6): history flood\n7): clever cloak\n8): launch 3 proxys\n9): launch nebula\n10): source code\n11): proxy doc\n12): URL proxy\n13): proxy all in one\n15): launch Aluben\n16): launch procky list\n17): launch Aluben hub\n18): rename tab\n19:) launch ultraviolet\n20): launch holy ub\n21): tiny url"
 );
 if (option == "1") {
   function cloaking() {
@@ -400,6 +400,10 @@ if (option == "1") {
     "position:fixed;width:100vw;height:100vh;top:0px;left:0px;right:0px;bottom:0px;z-index:2147483647;background-color:white;border:none;";
   iframe.src = url;
   win.document.body.appendChild(iframe);
+} else if (option == "21") {
+  void open(
+    "http://tinyurl.com/create.php?url=" + encodeURIComponent(location.href)
+  );
 } else {
   alert("please enter a valid option");
 }
